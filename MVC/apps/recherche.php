6b6a5a70719c61d2f2a80a76cb9416a4f19c00b2
zzpapy <gregory.pace@hotmail.fr>
2016-08-24 16:54:24 +0200

    version 1.6

--- MVC/apps/recherche.php
+++ MVC/apps/recherche.php
@@ -1,46 +1,5 @@
 <?php 
-<<<<<<< HEAD
 
-
-
-=======
->>>>>>> 0539bdb924cb185fc10137b239c61a9e71c8afbf
-		
-	$depart = "";
-	$destination = "";
-	$choixdepart = "";
-	$choixdestination = "";
- 
-
-	if (!isset($_POST['depart']))
-	{
-		$error = 'cocher le sesk wesh';
-	} else {
-		$depart = $_POST['depart'];
-	}
-
-	
-	if (!isset($_POST['destination']))
-	{
-		$error = 'cocher le sesk wesh';
-	} else {
-		$destination = $_POST['destination'];
-	}
-
-
-	$json = file_get_contents('voyage.json');
-	$temp = json_decode($json, true);
-	$list = [];
-<<<<<<< HEAD
-	foreach ($temp as $voyage) {
-		  
-		if ($voyage['depart'] == $depart && $voyage['destination'] == $destination) {
-=======
->>>>>>> 90d72e0d4cd810c72a552ed4dc4b159410c78da2
-	
-	$i = 0;
-	
-<<<<<<< HEAD
 	$choixdepart = "";
 	$choixdestination = "";
 	
@@ -68,7 +27,7 @@
 		
 		$i++;
 		
-=======
+
 	// var_dump($_POST['depart'], $_POST['destination']);
 
 	while ($i < sizeof($temp)) {
@@ -90,7 +49,7 @@
 
 	if (empty($list)) {
 		$error = " Aucun départ ou destination n'est répertoriée";
->>>>>>> 90d72e0d4cd810c72a552ed4dc4b159410c78da2
+
 	}
 	
 

92baf40d522a130375f057e8ca4d57c4ea1c6725
zzpapy <gregory.pace@hotmail.fr>
2016-08-29 15:44:01 +0200

    trait voyage

--- MVC/apps/traitement_voyage.php
+++ MVC/apps/traitement_voyage.php
@@ -3,11 +3,11 @@
 	if ( isset($_POST["action"],$_POST["id"]) && $_POST["action"]=="comment")
 	{
 			
-		$user_id=$_SESSION["email"];
+		$user_id=mysqli_real_escape_string($_SESSION["email"]);
 		var_dump("trait".$_POST);
 		
-		$comment = $_POST["comment"];
-		$voyage_id = $_POST["id"];		
+		$comment = mysqli_real_escape_string($_POST["comment"]);
+		$voyage_id = intval($_POST["id"]);
 		$query = "INSERT INTO comments (comment,id_user,id_voyage) VALUES ('".$comment."','".$user_id."','".$voyage_id."')";
 		mysqli_query($db, $query);
 		$id = mysqli_insert_id($db);	

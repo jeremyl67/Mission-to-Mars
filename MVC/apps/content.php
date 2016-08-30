11003bf9c021c4cd4443e3254556188ebb2da74a
zzpapy <gregory.pace@hotmail.fr>
2016-08-30 09:47:03 +0200

    comment/note

--- MVC/apps/content.php
+++ MVC/apps/content.php
@@ -1,1 +1,20 @@
-<?php require 'views/content.phtml';?>
+<?php
+	
+	$ask = mysqli_query($db,' SELECT * FROM comments WHERE id_voyage='.$voyage["id"] );
+	
+	
+	while($comments = mysqli_fetch_assoc($ask))
+	{
+		require 'views/comment.phtml';
+	}
+	
+	
+		
+		
+	
+	
+	
+		
+	
+		
+?>
